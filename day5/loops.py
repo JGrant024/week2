9dab462fda3cd139f868b4bc4d92dd7cc9def744
JGrant024 <jonathan.grant23@icloud.com>
2024-01-16 16:04:46 -0500

    created variables and a while loop to iterate over each number from the users input

--- day5/loops.py
+++ day5/loops.py
@@ -6,3 +6,17 @@
 # Can you write a loop that increments by more than 1?
 # Can you create a range of numbers from which the user can choose?
 # Can you let the user know when they choose something out of that range?
+
+#creates a variable that asks the user for their starting number. 
+start_number = int(input("What is your starting number? "))
+
+
+#creates a varibale that asks for the user for their ending number. 
+end_number = int(input("What is your ending number? ")) 
+
+
+#created a while loop that reads while the start number is less than the ending number displays the current number and its value adding 1 to each line iterated over. 
+while start_number < end_number: 
+        print("Current number", start_number)
+        start_number += 1 
+print("Loop completed! ")
