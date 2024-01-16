0df4686b3b41fb18a6e1b048c75ec92f5048f30d
JGrant024 <jonathan.grant23@icloud.com>
2024-01-16 15:53:04 -0500

    updated list + conditional excercise with conditional statements

--- day5/list_conditional.py
+++ day5/list_conditional.py
@@ -4,4 +4,22 @@
 # If they're not in the list, add them.
 # Can you write a condition that does both of these checks?
 
-# Think like this: "If the person exists, then remove them. Otherwise, if they don't, add them.
+# Think like this: "If the person exists, then remove them. Otherwise, if they don't, add them.\
+# Print the updated list. 
+
+# Make a new list containing members of your favorite band/sports team/television show.
+favorite_players = ["Kobe Bryant", "Jonathan Grant", "Michael Jordan"]
+
+# Person to check
+person_to_check = "Michael Jordan"
+
+# Write a conditional statement to check if a specific person is in that list.
+if person_to_check in favorite_players:
+    # If they are in the list, remove them.
+    favorite_players.remove(person_to_check)
+else:
+    # If they're not in the list, add them.
+    favorite_players.append(person_to_check)
+
+# Print the updated list.
+print("Updated List:", favorite_players)
