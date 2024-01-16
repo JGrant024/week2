d87aebd97c79d1efc1440533fd23511e67c41ca8
JGrant024 <jonathan.grant23@icloud.com>
2024-01-16 14:42:14 -0500

    updated strings to list excercise, and added comments for additional notes

--- day5/string_to_list.py
+++ day5/string_to_list.py
@@ -5,3 +5,29 @@
 # Create a new variable that is an empty string (just like in JavaScript).
 # Loop through the list, for each letter in the list, add it to the new string your created.
 # Print out the new string. It should be the reversed version of the string you created. i.e. "sean" -> "neas".
+
+#Created a string with at least 6 characters
+string_name = "coding"
+
+#empty list 
+empty_list = [] 
+
+# for loop that iterates over each letter in the string 
+for letter in string_name:
+    empty_list.append(letter) 
+
+
+#reverses list 
+empty_list.reverse() 
+
+#empty string 
+empty_string = ""
+
+
+#for loop that iterates over each element in the list
+for letter in empty_list:   
+    empty_string += letter 
+
+
+print("Reversed String",empty_string)
+
