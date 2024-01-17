0ce416534b360933ab972b938b625256dd14bed7
JGrant024 <jonathan.grant23@icloud.com>
2024-01-17 08:17:52 -0500

    created functions for conversions

--- day5/conversions.py
+++ day5/conversions.py
@@ -10,4 +10,25 @@
 
 # C = (F - 32) * 5/9
 
-# Write a function that takes a temperature in Fahrenheit, converts it to Celsius, and returns the value.
+# Write a function that takes a temperature in Fahrenheit, converts it to Celsius, and returns the value.åç
+
+
+
+# Celsius to Fahrenheit
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+# Fahrenheit to Celsius
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+
+# Examples
+celsius_temperature = 25
+converted_fahrenheit = celsius_to_fahrenheit(celsius_temperature)
+print(f"{celsius_temperature} degrees Celsius is {converted_fahrenheit:.2f} degrees Fahrenheit.")
+
+fahrenheit_temperature = 77
+converted_celsius = fahrenheit_to_celsius(fahrenheit_temperature)
+print(f"{fahrenheit_temperature} degrees Fahrenheit is {converted_celsius:.2f} degrees Celsius.")
