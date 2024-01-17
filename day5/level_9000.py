88e4ec026d73a29161a6abb9c778088fdf6b3674
JGrant024 <jonathan.grant23@icloud.com>
2024-01-17 14:42:44 -0500

    updated questions in level 9000

--- day5/level_9000.py
+++ day5/level_9000.py
@@ -59,3 +59,66 @@
 # It would return 133.59
 
 # As before, consider writing helper functions to deal with the bills and the coins separately.
+
+
+
+
+                                         #step 1.) find the difference between the amount total, and the amount paid. 
+                                         #step 2.) Split the difference into bills and coins 
+                                         #step 3.) Create or call the functions (run the other functions count_bills, count_coins ) 
+                                         #step 4.) That the result of those two functions and return a tuple. 
+     
+def make_change(total_charge, payment):
+    #find the difference 
+    difference = payment - total_charge
+    #Split the difference into bills and coins 
+
+    difference_as_string = str(difference)
+    parts = difference_as_string.split(".")
+    #Define the bills and coins bacsed on the parts
+    bills = parts[0]
+    coins = parts[1]
+    return count_bills(bills), count_coins(coins)
+
+def count_bills(payment_in_bills):
+    payment_in_bills = int(payment_in_bills)
+
+    hundreds = payment_in_bills //100 
+    payment_in_bills %= 100 
+
+    fifties = payment_in_bills //50
+    payment_in_bills %= 50
+
+    twenties  = payment_in_bills //20
+    payment_in_bills %= 20
+
+    tens = payment_in_bills //10
+    payment_in_bills %= 10
+
+    fives = payment_in_bills //5
+    payment_in_bills %= 5 
+
+    singles = payment_in_bills //1
+    payment_in_bills %= 1
+    return(singles, fives, tens, twenties, fifties, hundreds)
+    
+
+def count_coins(payment_in_coins):
+    payment_in_coins = int(payment_in_coins)
+
+    quarters = payment_in_coins// 25
+    payment_in_coins %= 25
+
+    dimes = payment_in_coins// 10
+    payment_in_coins %= 10
+
+    nickels  = payment_in_coins// 5
+    payment_in_coins %= 5
+
+    pennies = payment_in_coins// 1
+    payment_in_coins %= 1
+    return (quarters, dimes, nickels, pennies)
+
+
+# you can turn predicatable inputs into a function 
+print(make_change(148.49, 200)) 
