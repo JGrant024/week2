2f3491ae42f4f67ca9613b7ca02828a90cd5e788
JGrant024 <jonathan.grant23@icloud.com>
2024-01-17 08:16:56 -0500

    updated notes

--- day5/list_conditional.py
+++ day5/list_conditional.py
@@ -13,7 +13,7 @@ favorite_players = ["Kobe Bryant", "Jonathan Grant", "Michael Jordan"]
 # Person to check
 person_to_check = "Michael Jordan"
 
-# Write a conditional statement to check if a specific person is in that list.
+# created conditional statements using if else to check if players are in the list, and how to add and remove them from list. 
 if person_to_check in favorite_players:
     # If they are in the list, remove them.
     favorite_players.remove(person_to_check)
@@ -21,5 +21,5 @@ else:
     # If they're not in the list, add them.
     favorite_players.append(person_to_check)
 
-# Print the updated list.
+# Print the updated list, passing in favorite_players as arguement 
 print("Updated List:", favorite_players)
