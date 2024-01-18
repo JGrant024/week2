b2a51ee6b0f9b6847800b3f4e4bcf40aa962cc9b
JGrant024 <jonathan.grant23@icloud.com>
2024-01-17 22:47:54 -0500

    Instantiated instances over objects

--- day6/person.py
+++ day6/person.py
@@ -15,3 +15,26 @@
 # Have jordan greet sonny using the greet method.
 # Write a print statement to print the contact info (email and phone) of Sonny.
 # Write another print statement to print the contact info of Jordan.
+
+
+class Person:
+    def __init__(self, name, email, phone):
+      self.name = name
+      self.email = email
+      self.phone = phone
+
+    def greet(self, other_person):
+      print('Hello %s, I am %s!' % (other_person.name, self.name))
+
+sonny = Person('Sonny', 'sonny@hotmail.com', '483-485-4948')
+
+jordan = Person('Jordan', 'jordan@aol.com', '495-586-3456')
+
+
+sonny.greet(jordan) 
+jordan.greet(sonny)
+
+#Creates print statement using string interpolation by modolos
+print("Sonny's contact info: Email - %s, Phone - %s" % (sonny.email, sonny.phone))
+print("Jordan's contact info: Email - %s, Phone- %s" % (jordan.email, jordan.phone))
+
